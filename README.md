f5a3b074a92954d6a2f0ee0aae2b0985b91ebfdc
Bright Udoette <brightudoette77@gmail.com>
2023-10-09 13:56:09 +0100

    Updated the canvas eventlisteners to support touch devices

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
-##Paint App
-[Click Me to goto the webpage]()
+## Paint App
 
-###Major Takeaway
+[Click Me to access the webpage](https://tangerine-raindrop-d21767.netlify.app/)
+
+### Major Takeaway
 
 - Learnt how to work with the canvas, draw on the canvas and other fun activities that can be done on the canvas.
 
 - Functional Programming in javascript
 
-###Future Contributions
+### Future Contributions
 
 - Enabling drawing ability for touchscreen devices
 - Adding more drawing features to the screen.
--- paint_app.js
+++ paint_app.js
@@ -5,11 +5,9 @@ const saveBtn = document.querySelector(".btn--save");
 const clearBtn = document.querySelector(".btn--clear");
 const imagePreview = document.querySelector(".imagePreview");
 
-// Set canvas dimensions using CSS properties
-canvas.style.width = "600px"; // Set the width as desired
-canvas.style.height = "400px"; // Set the height as desired
-
-
+// Set internal dimensions for the canvas
+canvas.width = 600;
+canvas.height = 400;
 
 const coordinate = {
     draw: false,
@@ -17,25 +15,46 @@ const coordinate = {
     y: 0,
     lastX: 0,
     lastY: 0,
-}
+};
 
-const canvasRect = canvas.getBoundingClientRect();
 canvas.addEventListener("mousemove", (e) => {
     coordinate.lastX = coordinate.x;
     coordinate.lastY = coordinate.y;
-    coordinate.x = e.clientX - canvasRect.left;
-    coordinate.y = e.clientY - canvasRect.top;
-
+    coordinate.x = e.clientX - canvas.getBoundingClientRect().left;
+    coordinate.y = e.clientY - canvas.getBoundingClientRect().top;
     draw();
 });
-canvas.addEventListener("mousedown", (e) => {
-    e.preventDefault();
+
+canvas.addEventListener("mousedown", () => {
     coordinate.draw = true;
 });
+
 canvas.addEventListener("mouseup", () => {
     coordinate.draw = false;
 });
+
 canvas.addEventListener("mouseout", () => {
+    coordinate.draw = false;
+});
+
+canvas.addEventListener("touchstart", (e) => {
+    e.preventDefault();
+    coordinate.draw = true;
+    coordinate.x = e.touches[0].clientX - canvas.getBoundingClientRect().left;
+    coordinate.y = e.touches[0].clientY - canvas.getBoundingClientRect().top;
+});
+
+canvas.addEventListener("touchmove", (e) => {
+    if (coordinate.draw) {
+        coordinate.lastX = coordinate.x;
+        coordinate.lastY = coordinate.y;
+        coordinate.x = e.touches[0].clientX - canvas.getBoundingClientRect().left;
+        coordinate.y = e.touches[0].clientY - canvas.getBoundingClientRect().top;
+        draw();
+    }
+});
+
+canvas.addEventListener("touchend", () => {
     coordinate.draw = false;
 });
 
@@ -43,38 +62,41 @@ clearBtn.addEventListener("click", clearCanvas);
 saveBtn.addEventListener("click", saveImg);
 
 function draw() {
-    if(coordinate.draw) {
-        
+    if (coordinate.draw) {
         ctx.beginPath();
         ctx.moveTo(coordinate.lastX, coordinate.lastY);
         ctx.lineTo(coordinate.x, coordinate.y);
         ctx.strokeStyle = penColor.value;
         ctx.lineWidth = penWidth.value;
-        ctx.lineCap = "round"
+        ctx.lineCap = "round";
         ctx.stroke();
         ctx.closePath();
     }
 }
-function clearCanvas(){
-        let clearStatus = confirm("Clear canvas?");
-        if(clearStatus){
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            imagePreview.innerHTML = "";
-        }
+
+function clearCanvas() {
+    let clearStatus = confirm("Clear canvas?");
+    if (clearStatus) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        imagePreview.innerHTML = "";
     }
+}
+
 function saveImg() {
-        const dataURL = canvas.toDataURL();
-        // console.log(dataURL);
-        const img = document.createElement("img"); // create an image element
-        imagePreview.prepend(img); // add the image element as the first element of the 'output' div
-        img.setAttribute("src", dataURL);
-        const link = document.createElement("a");
-        imagePreview.append(link);
-        let userFileName = prompt("Enter your file name", "Name");
-        let fileName = userFileName + ".png";
-        link.setAttribute("download", fileName);
-        link.href = dataURL;
-        link.click();
-        imagePreview.removeChild(link);
-        imagePreview.removeChild(img);
-      }
+    const dataURL = canvas.toDataURL();
+    const img = document.createElement("img");
+    imagePreview.prepend(img);
+    img.setAttribute("src", dataURL);
+    const link = document.createElement("a");
+    imagePreview.append(link);
+    let userFileName = prompt("Enter your file name", "Name");
+    if (userFileName == null || userFileName == undefined) {
+        return;
+    }
+    let fileName = userFileName + ".png";
+    link.setAttribute("download", fileName);
+    link.href = dataURL;
+    link.click();
+    imagePreview.removeChild(link);
+    imagePreview.removeChild(img);
+}
